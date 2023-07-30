c299990a958779dfec8d874b539f9d58144cbffb
Whiskydog <alan.varde@gmail.com>
2023-07-30 00:27:49 -0300

    Fix Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   plugins: [react()],
+  base: '/frontendmentor-interactive-rating'
 })
